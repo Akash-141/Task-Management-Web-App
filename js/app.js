39f457202afdd8798bcf3f13ae820dbde73787d2
Akash Paul <akash17.paul@gmail.com>
2026-07-13 09:56:36 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@
         let unsubscribeFromTasks = null;
         let lastDeletedTask = null;
 
+
+
+        
         // ===== TASK MANAGER =====
         class TaskManager {
             constructor() {
