96df9e463226d8f2a368167bb95160cadbacded8
Akash Paul <akash17.paul@gmail.com>
2026-09-09 10:34:13 +0600

    Refactor authentication page logic

--- js/auth.js
+++ js/auth.js
@@ -1,17 +1,6 @@
 // Authentication Page Logic
 (function() {
     'use strict';
-
-
-
-
-
-
-
-
-
-
-
     
     // ===== STATE =====
     let currentForm = 'login'; // 'login', 'signup', or 'reset'
